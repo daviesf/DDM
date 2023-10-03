741ebed5e91d8cab1fc7e1863c7c5822c4030e5c
Samuka <samuelpcamargo2005@gmail.com>
2023-10-03 11:44:33 -0300

    Create cadastro.dart

---
+++ crud/lib/cadastro.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyPedido extends StatefulWidget {
+  const MyPedido({super.key});
+
+  @override
+  State<MyPedido> createState() => _MyPedidoState();
+}
+
+class _MyPedidoState extends State<MyPedido> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
